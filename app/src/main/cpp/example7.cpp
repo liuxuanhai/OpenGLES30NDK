20c0ce8e952c3db8236df0d193b3e54eb6bcec86
Raining <tian-c@qq.com>
2019-08-23 20:19:03 +0800

    增加坐标系操作demo

--- app/src/main/cpp/example7.cpp
+++ app/src/main/cpp/example7.cpp
@@ -1,3 +1,155 @@
 //
 // Created by Raining on 2019/8/21.
+// 坐标系变换
 //
+
+#include <GLES3/gl3.h>
+#include <jni.h>
+#include "esUtils.h"
+#include "example.h"
+#include <cstdlib>
+#include <ctime>
+#include "image/ImageUtils.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+float vertices[] = {
+        //---- 位置 ----  ---- 颜色 ----     - 纹理坐标 -
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // 右上
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,   // 左下
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f    // 左上
+};
+
+unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+};
+
+static GLuint program = 0;
+static GLuint vao = 0;
+static GLuint texture1 = 0;
+static GLuint texture2 = 0;
+
+JNIEXPORT void JNICALL surfaceCreated(JNIEnv *env, jobject obj, jobject context, jint color) {
+    color = 0xffff8000;
+    GLfloat alphaF = ((color >> 24) & 0xFF) * 1.0f / 255;
+    GLfloat redF = ((color >> 16) & 0xFF) * 1.0f / 255;
+    GLfloat greenF = ((color >> 8) & 0xFF) * 1.0f / 255;
+    GLfloat blueF = (color & 0xFF) * 1.0f / 255;
+    glClearColor(redF, greenF, blueF, alphaF);
+
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    jobject bmp = readAssetImage(env, context, "wall.jpg");
+    Image8888 img;
+    img.SetImage(env, bmp);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.m_width, img.m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                 img.m_pDatas);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    img.ClearAll();
+    env->DeleteLocalRef(bmp);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    bmp = readAssetImage(env, context, "awesomeface.png");
+    img.SetImage(env, bmp);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.m_width, img.m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                 img.m_pDatas);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    img.ClearAll();
+    env->DeleteLocalRef(bmp);
+
+    char *vertexShaderCode = (char *) readAssetFile(env, context, "vertex3.glsl", true);
+    char *fragmentShaderCode = (char *) readAssetFile(env, context, "fragment3.glsl", true);
+    program = createProgram(vertexShaderCode, fragmentShaderCode);
+    free(vertexShaderCode);
+    free(fragmentShaderCode);
+
+    if (program) {
+        GLuint vbo;
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        GLuint ebo;
+        glGenBuffers(1, &ebo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) 0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                              (void *) (3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                              (void *) (6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+        glBindVertexArray(0);
+
+        glUseProgram(program);
+        glUniform1i(glGetUniformLocation(program, "ourTexture1"), 0);
+        glUniform1i(glGetUniformLocation(program, "ourTexture2"), 1);
+    }
+}
+
+static int sWidth = 1;
+static int sHeight = 1;
+
+JNIEXPORT void JNICALL surfaceChanged(JNIEnv *env, jobject obj, jint width, jint height) {
+    sWidth = width;
+    sHeight = height;
+    glViewport(0, 0, width, height);
+}
+
+JNIEXPORT void JNICALL drawFrame(JNIEnv *env, jobject obj) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    if (program) {
+        glUseProgram(program);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+        glm::mat4 view = glm::mat4(1.0f);
+        //注意，我们将矩阵向我们要进行移动场景的反方向移动。
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+        //base_LOG("w=%d,h=%d", sWidth, sHeight);
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), (float) sWidth / (float) sHeight,
+                                      0.1f, 100.0f);
+
+        glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE,
+                           glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_FALSE,
+                           glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE,
+                           glm::value_ptr(projection));
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    }
+}
